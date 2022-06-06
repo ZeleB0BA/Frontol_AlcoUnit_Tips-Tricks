8caaf683640d2a5d66fb1181ed0ec7ee78aa4e0f
3e/\e BoBa <57889575+ZeleB0BA@users.noreply.github.com>
2022-06-06 14:47:15 +0500

    Add files via upload

---
+++ change_APCode_on_production.sql
@@ -0,0 +1,8 @@
+select * from product_info where alc_code="0100000005410000016";
+
+ 
+UPDATE product_info 
+set product_v_code=2128
+where alc_code="0100000005410000016";
+
+select * from product_info where alc_code="0100000005410000016";
